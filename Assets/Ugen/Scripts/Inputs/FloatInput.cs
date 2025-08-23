45e509fb3189437e25c208137ff6dd45e1a01b1f
shivaduke28 <shivaduke28@gmail.com>
2025-08-23 22:29:30 +0900

    FloatInput, RandomInput

---
+++ Assets/Ugen/Scripts/Inputs/FloatInput.cs
@@ -0,0 +1,22 @@
+using R3;
+using UnityEngine;
+
+namespace Ugen.Inputs
+{
+    [AddComponentMenu("Ugen/Ugen Float Input")]
+    public sealed class FloatInput : UgenInput<float>
+    {
+        [SerializeField] SerializableReactiveProperty<float> _value = new();
+        [SerializeField] UgenInput<float> _input;
+
+        void Start()
+        {
+            if (_input != null)
+            {
+                _input.AsObservable().Subscribe(x => _value.OnNext(x));
+            }
+        }
+
+        public override Observable<float> AsObservable() => _value;
+    }
+}
---
+++ Assets/Ugen/Scripts/Inputs/RandomInput.cs
@@ -0,0 +1,25 @@
+using R3;
+using UnityEngine;
+
+namespace Ugen.Inputs
+{
+    [AddComponentMenu("Ugen/Ugen Random Input")]
+    public sealed class RandomInput : UgenInput<float>
+    {
+        [SerializeField] float _min = 0f;
+        [SerializeField] float _max = 1f;
+
+        [SerializeField] UgenInput<Unit> _update;
+
+        public override Observable<float> AsObservable()
+        {
+            if (_update != null)
+            {
+                return _update.AsObservable()
+                    .Select(_ => Random.Range(_min, _max));
+            }
+
+            return Observable.Return(Random.Range(_min, _max));
+        }
+    }
+}
