e335d27e6d8f012ae00bf65b69d8687ca93d6516
shivaduke28 <shivaduke28@gmail.com>
2025-08-24 01:54:58 +0900

    微調整

--- Assets/Ugen/Scripts/Editor/UgenInputSelectorPropertyDrawer.cs
+++ Assets/Ugen/Scripts/Editor/UgenInputSelectorPropertyDrawer.cs
@@ -6,19 +6,16 @@ using UnityEngine;
 namespace Ugen.Editor
 {
     [CustomPropertyDrawer(typeof(UgenInputSelectorAttribute))]
-    public class UgenInputSelectorPropertyDrawer : PropertyDrawer
+    public sealed class UgenInputSelectorPropertyDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
 
-            // ラベルを描画
             position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
-            // 現在の値を取得
             var currentInput = property.objectReferenceValue as UgenInput;
 
-            // 表示するテキストを作成
             var displayText = "None";
             if (currentInput != null)
             {
@@ -26,17 +23,14 @@ namespace Ugen.Editor
                 displayText = $"{currentInput.gameObject.name} ({componentTypeName})";
             }
 
-            // フィールドとボタンを配置
-            var fieldRect = new Rect(position.x, position.y, position.width - 22, position.height);
-            var buttonRect = new Rect(position.x + position.width - 20, position.y, 20, position.height);
+            // 全体をボタンとして表示
+            var buttonStyle = new GUIStyle(GUI.skin.button)
+            {
+                alignment = TextAnchor.MiddleLeft,
+                padding = new RectOffset(8, 8, 2, 2)
+            };
 
-            // 読み取り専用風のフィールド表示
-            GUI.enabled = false;
-            EditorGUI.TextField(fieldRect, displayText);
-            GUI.enabled = true;
-
-            // 選択ボタン
-            if (GUI.Button(buttonRect, "..."))
+            if (GUI.Button(position, displayText, buttonStyle))
             {
                 UgenInputSelectionWindow.ShowWindow((selectedInput) =>
                 {
