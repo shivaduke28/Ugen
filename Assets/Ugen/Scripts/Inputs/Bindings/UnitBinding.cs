0a8febcaf6d693be9bc75a22935c0cd1e8448dc1
shivaduke28 <shivaduke28@gmail.com>
2025-08-24 00:10:13 +0900

    UnitBindingを追加

---
+++ Assets/Ugen/Scripts/Inputs/Bindings/UnitBinding.cs
@@ -0,0 +1,31 @@
+using System;
+using R3;
+using Ugen.Inputs;
+using UnityEngine;
+
+namespace Ugen.Bindings
+{
+    [Serializable]
+    public sealed class UnitBinding
+    {
+        [SerializeField] UgenInput _input;
+
+        public Observable<Unit> AsObservable()
+        {
+            return _input switch
+            {
+                UgenInput<Unit> input => input.AsObservable(),
+                UgenInput<bool> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<int> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<uint> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<float> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<double> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<Vector2> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<Vector3> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<Vector4> input => input.AsObservable().AsUnitObservable(),
+                UgenInput<Color> input => input.AsObservable().AsUnitObservable(),
+                _ => Observable.Never<Unit>()
+            };
+        }
+    }
+}
