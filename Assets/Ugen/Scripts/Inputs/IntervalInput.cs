b444cfc7151725629ae6bd9f437fb2a42c5e5d83
shivaduke28 <shivaduke28@gmail.com>
2025-08-24 02:37:35 +0900

    Intervalを動的に変更できるようにした

--- Assets/Ugen/Scripts/Inputs/IntervalInput.cs
+++ Assets/Ugen/Scripts/Inputs/IntervalInput.cs
@@ -1,17 +1,31 @@
 using System;
 using R3;
+using Ugen.Bindings;
 using UnityEngine;
 
 namespace Ugen.Inputs
 {
     public sealed class IntervalInput : UgenInput<Unit>
     {
-        [SerializeField] float _intervalSec = 1f;
+        [SerializeField] SerializableReactiveProperty<float> _interval = new(1f);
+        [SerializeField] FloatBinding _intervalBinding;
 
+        void Start()
+        {
+            if (_intervalBinding != null)
+            {
+                _intervalBinding.AsObservable()
+                    .Where(interval => interval >= 0) // 負の値は無視
+                    .Subscribe(interval => _interval.Value = interval)
+                    .AddTo(this);
+            }
+        }
 
         public override Observable<Unit> AsObservable()
         {
-            return Observable.Interval(TimeSpan.FromSeconds(_intervalSec));
+            return _interval
+                .Select(interval => interval == 0 ? Observable.EveryUpdate() : Observable.Interval(TimeSpan.FromSeconds(interval)))
+                .Switch();
         }
     }
 }
